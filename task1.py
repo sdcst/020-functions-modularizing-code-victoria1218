501ee99c0018c6d1012d3a5059cc68d4bfe332fb
victoria1218 <zhangshuyi682@gmail.com>
2024-05-02 12:26:12 -0700

    20

--- task1.py
+++ task1.py
@@ -1,6 +1,19 @@
 def A():
 
+    print("hello")
+
 def B():
 
+    print("how are you")
 
 def C():
+
+    print("hi")
+
+x = str(input("enter a letter "))
+if x == "A":
+    A()
+elif x == "B":
+    B()
+elif x == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,27 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+
+def title():
+    print("choose a random number between 1 and 100,guess the number in 10 turns")
+title()
+def game():
+    x = 64
+    c = 0 
+    y = int(input("enter a number"))
+    while x != y:
+        if x < y :
+            print("over")
+            y = int(input("enter a number"))
+            c += 1 
+        else:
+            print("under")
+            y = int(input("enter a number"))
+            c += 1
+        if c == 10:
+            print("You are out of chance!")
+            break
+    if x == y:
+        print("You got it!")
+game()
